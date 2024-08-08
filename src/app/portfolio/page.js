d983052f3fe887ecb41e2145279d2100ab1746da
rzkidd <rezasaputra878@student.uns.ac.id>
2024-08-09 06:27:30 +0700

    fix portfolio

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -5,7 +5,7 @@ import { faCircleChevronUp, faEnvelope } from "@fortawesome/free-solid-svg-icons
 import { Footer } from "flowbite-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
 
 export default function Home() {
@@ -42,29 +42,31 @@ export default function Home() {
   // setFiles(total)
 
   return (
-    <main className="flex min-h-screen flex-col bg-color-dark-green">
-      <Heading />
-      <a href="#" className="fixed right-5 bottom-5">
-        <FontAwesomeIcon icon={faCircleChevronUp} className="text-5xl text-color-light-green"/>
-      </a>
-      
-      <div className="w-full flex flex-col items-center " >
-        <h2 className="text-3xl font-semibold text-color-green mt-10">{portfolio.name}</h2>
-        <div className="my-10 grid grid-cols-1 gap-5 w-4/5">
-          <img src={portfolio.link} className="w-full rounded" />
-          <div className="mt-5 grid grid-cols-2 gap-5">
-            {
-              total.map((file) => 
-                <img src={file} key={file} className="w-full rounded" />
-              )
-            }
+    <Suspense>
+      <main className="flex min-h-screen flex-col bg-color-dark-green">
+        <Heading />
+        <a href="#" className="fixed right-5 bottom-5">
+          <FontAwesomeIcon icon={faCircleChevronUp} className="text-5xl text-color-light-green"/>
+        </a>
+        
+        <div className="w-full flex flex-col items-center " >
+          <h2 className="text-3xl font-semibold text-color-green mt-10">{portfolio.name}</h2>
+          <div className="my-10 grid grid-cols-1 gap-5 w-4/5">
+            <img src={portfolio.link} className="w-full rounded" />
+            <div className="mt-5 grid grid-cols-2 gap-5">
+              {
+                total.map((file) => 
+                  <img src={file} key={file} className="w-full rounded" />
+                )
+              }
+            </div>
           </div>
         </div>
-      </div>
 
-      <Footer container className="bg-color-med-green/30">
-        <Footer.Copyright href="#" by="Reza Akbar Saputra" year={2024} className="text-color-green text-lg" />
-      </Footer>
-    </main>
+        <Footer container className="bg-color-med-green/30">
+          <Footer.Copyright href="#" by="Reza Akbar Saputra" year={2024} className="text-color-green text-lg" />
+        </Footer>
+      </main>
+    </Suspense>
   );
 }
